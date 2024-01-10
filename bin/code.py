4c40ba23c1f51708577d88f15dd5801db2f741b3
qiqi <i@timqi.com>
2024-01-11 07:37:22 +0800

    update

--- bin/code.py
+++ bin/code.py
@@ -220,7 +220,7 @@ def patchpilot(token):
                 "headers:{Authorization:`token %s`" % token,
             )
             r = r.replace(
-                "return r.devOverride?.copilotTokenUrl??this.tokenUrl",
+                "return n.devOverride?.copilotTokenUrl??this.tokenUrl",
                 'return "https://mgithub.cc/apis/ed_gh_stu.i/copilot_internal/v2/token"',
             )
             with open(p, "w") as f:
